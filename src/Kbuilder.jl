aab2a66583344841fa443a8b05ee54388a7a954b
Jarrod Angove <jangove@ualberta.ca>
2024-08-12 15:49:40 -0600

    Wrote GPU kernel for construction covariance matrices in GPAR. I think it's working...

--- src/Kbuilder.jl
+++ src/Kbuilder.jl
@@ -1,47 +1,98 @@
-using KernelAbstractions, AMDGPU, LinearAlgebra
+using AMDGPU, LinearAlgebra, KernelAbstractions
 
-function build_K(X::AbstractMatrix{<:Real}, times::AbstractVector{<:Real}, N::Int, n::Int)
-    T = KernelAbstractions.zeros(get_backend(X), N, N, n)
-    @kernel function build_tensor!(T, @Const(X), @Const(times))
-        i,j,k = @index(Global, NTuple)
-        T[i,j,k] = i + j + k
+function get_test_x(n, N)
+    times = collect(1:n)./10.0
+    X = Matrix{Float32}(undef, n, N)
+    for i in 1:N
+    X[:, i] = sqrt(i) .* times .+ randn()/20
     end
-    return T
+    return X
 end
+export get_test_x
 
-function build_tensor!(X , Δt)
+function build_tensor(X , Δt)
+    # T likely needs to be a N,N,n,d matrix if 
+    # you want to use input with more dimensions
     n, N = size(X)
     T = AMDGPU.zeros(N,N,n)
     @kernel function build_tensor_kernel!(T, @Const(X))
         i,j,k = @index(Global, NTuple)
         
-        # The lower and upper triangular portions
-        # will be equal so we don't need to calc both
-        #if j ≥ i
-        @inbounds T[i,j,k] = (abs(X[k, i] - X[k, j])) * Δt
-        #end
-        
-        for m in 2:size(X)[1]
-            @inbounds T[i,j,m] = T[i,j,m] + T[i, j, m - 1]
-            @synchronize
-        end
+        @inbounds T[i,j,k] = (X[k, i] - X[k, j])^2 * Δt
     end
 
     backend=KernelAbstractions.get_backend(T)
     kernel! = build_tensor_kernel!(backend)
     kernel!(T, X; ndrange = size(T))
     
-    # This is the slowest possible way to do this
-    # Look up "parallel prefix sum kernel" if this
-    # is a bottleneck
-    #for k in 2:size(T)[3]
-    #        T[:,:,k] = T[:,:,k] .+ T[:,:,k-1]
-    #end
+    @kernel function scan_sum_kernel!(T, T_out)
+        i,j = @index(Global, NTuple)
+        #k = @index(Local, Linear)
+        @inbounds for k in 1:n
+            if k == 1
+                T_out[i,j,k] = T[i,j,k]
+            else
+                T_out[i,j,k] = T[i,j,k] + T_out[i,j,k-1]
+            end
+            @synchronize
+        end
+    end
+    scan_sum_kernel! = scan_sum_kernel!(backend)
 
-    #for k in 1:size(T)[3]
-    #    cholesky!(T[:,:,k])
-    #end
-    return T
+    T_out = KernelAbstractions.zeros(backend, Float32, N,N,n)
+    scan_sum_kernel!(T,T_out; ndrange=(N,N))
+    KernelAbstractions.unsafe_free!(T)
+
+    return T_out
 end
-export build_tensor!
+export build_tensor
+
+function build_tensor_cpu(X::AbstractArray{Float32}, Δt)::AbstractArray{Float32, 3}
+    n,N = size(X)
+    T = [(X[k,i] - X[k,j])^2 * Δt for i in 1:N, j in 1:N, k in 1:n]
+    T_out = zeros(size(T)...)
+    for k in 1:n
+        if k == 1
+            T_out[:,:,k] .= T[:,:,k]
+        else
+            T_out[:,:,k] .= T[:,:,k] .+ T_out[:,:, k-1]
+        end
+    end
+    T_out = exp.(-0.5 .* T_out/100.0)
+
+    return T_out
+end
+export build_tensor_cpu
+
+# Untested!!!
+function build_K(X, t, θ)
+    dt = diff(t); 
+    for i in eachindex(dt)
+        if dt[i] !== dt[1]
+            throw("Times are not evenly spaced")
+        end
+    end
+    Δt = dt[1]
+    T = build_tensor(X, Δt)
+    K = θ[1]^2 .* exp.(-0.5 .* T ./θ[2]^2)
+    return K
+end
+
+# UNOPTIMIZED and untested!!!!!
+function lml_K(X, Y, t, θ)
+    n, N = size(X)
+    K = build_K(X, t, θ)
+    # Might want to move everything to shared memory here
+    logp = Vector{Float32}(undef, n)
+    for k in 1:n
+        y = Y[k, :]
+        L = cholesky(K[:,:,k])
+        α = L.U \ (L.L \ y)
+        logp[k] = -0.5 .* y' * α .- tr(L.L) .- n/2 * log(2π)
+    end
+    return logp
+end
+
+
+
 
